3911f331e49347fa694ac20a28ec90d633a237d5
Edward Tanguay (windows) <edward@tanguay.info>
2024-04-06 14:03:03 +0200

    still error

--- db.js
+++ db.js
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_ATLAS_CLUSTER_URL}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`;
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
 export const getAllTodos = () => {
 	return new Promise((resolve, reject) => {
 		try {
 			(async () => {
+				const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_ATLAS_CLUSTER_URL}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`;
+				const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
 				await mongoose.connect(uri, clientOptions);
 				const response = mongoose.connection.db.collection(process.env.MONGO_COLLECTION);
 				const todos = await response.find().toArray();
